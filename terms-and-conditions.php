26156450409d4ca5f346f3ed9e4d00a9138032a1
mrudulwazeefa <mrudul@wazeefa.in>
2025-07-08 16:00:02 +0530

    html to php convert files

---
+++ terms-and-conditions.php
@@ -0,0 +1,86 @@
+<?php include("includes/meta.php"); ?>
+  <title>Terms and Condition | Loanitol</title>
+</head>
+<body>
+ <?php include("includes/nav.php"); ?>
+    <div class="hero-small--section-area">
+      <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-lg-12">
+              <h6 class="h6-size col-sm-10 mb-0">
+                <span>Terms and Conditions</span>
+              </h6>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="container privacy">
+      <p>Welcome to Loanitol ("we," "our," or "us"). These Terms and Conditions ("Terms") govern your use of our website located at [www.loanitol.com] and any services we provide related to loan consultancy, comparison, and application assistance.</p>
+      <p>By accessing or using our Site and services, you agree to be bound by these Terms. If you do not agree to all the terms, please do not use our website or services.</p>
+      <h5>About Us</h5>
+      <p>Loanitol is a private limited company registered in India, providing loan consultancy and facilitation services in partnership with banks and NBFCs (Non-Banking Financial Companies). We assist customers in discovering, comparing, and applying for financial products including personal loans, business loans, home loans, property loan and more.</p>
+      <h5>Eligibility</h5>
+      <p>You must be:</p>
+      <ul>
+        <li>At least 18 years old.</li>
+        <li>Legally capable of entering into binding contracts under Indian law.</li>
+        <li>A resident of India.</li>
+      </ul>
+      <h5>Services Offered</h5>
+      <p>We provide:</p>
+      <ul>
+        <li>Loan product comparisons from multiple banks and NBFCs.</li>
+        <li>Application assistance for personal, business, home, Property and other loans.</li>
+        <li>Customer support and documentation guidance.</li>
+        <li>Lead generation and forwarding to financial institutions.</li>
+      </ul>
+      <p>Note: We are not a lender or a financial institution. Final approval and disbursement of any loan is solely at the discretion of the partnered banks/NBFCs.</p>
+<h5>No Guarantee of Loan Approval</h5>
+<p>We facilitate the loan application process but do not guarantee loan approval, amount, interest rate, or other terms. These are determined solely by the bank/NBFC after reviewing your application and eligibility.</p>
+<h5>User Responsibilities</h5>
+<p>By using our website and services, you agree to:</p>
+<ul>
+  <li>Provide accurate, complete, and up-to-date information.</li>
+  <li>Not engage in any fraudulent or illegal activities.</li>
+  <li>Comply with all applicable laws and regulations.</li>
+</ul>
+<p>Misrepresentation may lead to refusal of service or blacklisting.</p>
+<h5>Data Collection and Privacy</h5>
+<p>We collect and process your personal and financial data in accordance with our Privacy Policy. By using our services, you consent to such processing.</p>
+<p>Collected information may include:</p>
+<ul>
+  <li>Full name, contact number, and email ID</li>
+  <li>PAN, Aadhaar, and other KYC documents</li>
+  <li>Income details and bank statements</li>
+  <li>Credit score and loan history</li>
+</ul>
+<p>We may share your information with our partner financial institutions for processing your loan application.</p>
+<h5>Third-Party Links and Content</h5>
+<p>Our website may contain links to third-party websites, including banks and financial institutions. We are not responsible for the content, privacy policies, or practices of these websites. Use them at your own discretion</p>
+<h5>Intellectual Property</h5>
+<p>All content on the Site, including text, graphics, logos, and trademarks, is the property of Loanitol or its licensors and protected under intellectual property laws. You may not copy, reproduce, or use any content without prior written permission.</p>
+<h5>Limitation of Liability</h5>
+<p>To the maximum extent permitted by law, Loanitol will not be liable for any direct, indirect, incidental, or consequential damages arising from:</p>
+<ul>
+  <li>Use or inability to use our services</li>
+  <li>Delays in loan processing</li>
+  <li>Unauthorized access to user data</li>
+</ul>
+<h5>Indemnification</h5>
+<p>You agree to indemnify and hold harmless Loanitol, its affiliates, directors, and employees from any claim, demand, or damages resulting from your breach of these Terms or misuse of the services.</p>
+<h5>Changes to Terms</h5>
+<p>We reserve the right to update or modify these Terms at any time. Continued use of our website or services after such changes constitutes acceptance of the updated Terms.</p>
+<h5>Governing Law and Jurisdiction</h5>
+<p>These Terms are governed by the laws of India. Any disputes arising from or related to these Terms shall be subject to the exclusive jurisdiction of the courts of [Insert City].</p>
+<h5>Contact Us</h5>
+<p>If you have any questions or concerns about these Terms, please contact us at:</p>
+<h6>Loanitol</h6>
+<p>3rd Floor, V.E Arcade , Opp.Vyaparabavan , Palarivatam P.O ,Ernakulam  -682025 , Kerala</p>
+<span>Email: </span><a href="mailto:info@loanguru.in">info@loanguru.in</a>
+        <br>
+<span>Phone: </span><a href="tel:919090388883">+91-9090388883</a>
+</div>
+   <?php include("includes/calculation-bottom.php"); ?>
+   <?php include("includes/footer.php"); ?>
+</body>
+</html>
---
+++ working-capital-term-loan.php
@@ -0,0 +1,393 @@
+<?php include("includes/meta.php"); ?>
+  <title>Working Capital Term Loan | Loanitol </title>
+</head>
+<body>
+  <?php include("includes/nav.php"); ?>
+  <div class="hero-section-area">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+          <div class="col-lg-8 col-md-8">
+            <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                  Home</li>
+                <li class="breadcrumb-item">
+                MSME Loan</li>
+                <li class="breadcrumb-item">
+                  <span class="active">Working Capital Term Loan</span></li>
+              </ol>
+            </nav>
+
+
+            <h5 class="h5-size col-sm-12">Empowering Your Business with  <span>Reliable Capital</span>
+            </h5>
+            <p>Secure Working Capital Loans to support operations and fuel business growth.</p>
+          </div>
+          <div class="col-lg-4 col-md-4">
+              <img src="assets/services/msme-loan/working-capital-overdraft/banner.png" class="w-80" alt="Image">
+          </div>
+      </div>
+    </div>
+  </div>
+<!-- sticky nav -->
+<div class="sticky-nav-wrapper container" id="scrolling-nav">
+  <div class="container d-flex align-items-center justify-content-between">
+    <button class="arrow-btn d-flex align-items-center" onclick="scrollNav(-200)">
+      <img src="assets/services/msme-loan/icons/left-arrow.svg" alt="left-arrow" width="25" class="right-arrow">
+    </button>
+    <div class="box--style">
+      <div class="scroll-container">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link active" href="#overview">Overview</a></li>
+          <li class="nav-item"><a class="nav-link" href="#product-details">Features & Benefits</a></li>
+          <li class="nav-item"><a class="nav-link" href="#eligibility">Eligibility</a></li>
+          <li class="nav-item"><a class="nav-link" href="#document">Documents</a></li>
+          <li class="nav-item"><a class="nav-link" href="#faqs">FAQs</a></li>
+          <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+    <button class="arrow-btn d-flex align-items-center" onclick="scrollNav(200)">
+    <img src="assets/services/msme-loan/icons/right-arrow.svg" alt="right-arrow" width="25" class="left-arrow ">
+    </button>
+  </div>
+</div>
+<!--  -->
+<section id="overview" class="section">
+  <div class="empower-section">
+  <div class="container">
+    <h4 class="h4-size2">Overview</h4>
+    <p>One kind of business loan intended to give a lump sum of money to manage and enhance a company's daily operating demands is a working capital term loan. A working capital term loan is designed to fill short-to-medium-term cash flow shortages, as opposed to a standard term loan for long-term investments. The loan is a structured and predictable financing option because it is paid back over a defined period of time in fixed installments.  </p>
+  </div>
+  </div>
+</section>
+<section id="product-details" class="section">
+  <div class="details-section">
+    <div class="container">
+      <div class="col-lg-12">
+      <h4 class="mb-1 mb-4 h4-size2">Features & Benefits</h4>
+        <div class="row features-style">
+          <div class="col-md-4 col-sm-6">
+            <div class="features-style-box features-style-box-height">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-2">
+                  <div class="icon-style d-flex justify-content-center align-items-center">
+                    <img src="assets/services/msme-loan/icons/adaptable-credit-limit.svg" alt="logo" width="16">
+                  </div>
+                </div>
+                <div class="col-lg-10">
+                  <h6>Lump Sum Funding:</h6>
+                </div>
+              </div>
+              <p>Get a sizable sum of money up front, enabling quick access to finances.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="features-style-box features-style-box-height">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-2">
+                  <div class="icon-style d-flex justify-content-center align-items-center">
+                    <img src="assets/services/msme-loan/icons/revolving-facility.svg" alt="logo" width="16">
+                  </div>
+                </div>
+                <div class="col-lg-10">
+                  <h6>Structured Repayment:</h6>
+                </div>
+              </div>
+                <p>Cash flow management is predictable with fixed monthly or quarterly installments.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="features-style-box features-style-box-height">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-2">
+                  <div class="icon-style d-flex justify-content-center align-items-center">
+                    <img src="assets/services/msme-loan/icons/interest-only.svg" alt="logo" width="16">
+                  </div>
+                </div>
+                <div class="col-lg-10">
+                  <h6>Adaptable Use: </h6>
+                </div>
+              </div>
+              <p>The money can be applied to a range of working capital requirements, providing operational adaptability. </p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="features-style-box features-style-box-height">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-2">
+                  <div class="icon-style d-flex justify-content-center align-items-center">
+                    <img src="assets/services/msme-loan/icons/collateral-requirement.svg" alt="logo" width="16">
+                  </div>
+                </div>
+                <div class="col-lg-10">
+                  <h6>Better Cash Flow: </h6>
+                </div>
+              </div>
+                <p>Take care of immediate cash flow issues and keep business operations running smoothly.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="features-style-box features-style-box-height">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-2">
+                  <div class="icon-style d-flex justify-content-center align-items-center">
+                    <img src="assets/services/msme-loan/icons/renewable-facility.svg" alt="logo" width="15">
+                  </div>
+                </div>
+                <div class="col-lg-10">
+                  <h6>Longer Tenure Compared to Overdrafts: </h6>
+                </div>
+              </div>
+                <p>Term loans offer greater time for repayment due to their longer tenures.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="features-style-box features-style-box-height">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-2">
+                  <div class="icon-style d-flex justify-content-center align-items-center">
+                    <img src="assets/services/msme-loan/icons/boosts-credit.svg" alt="logo" width="15">
+                  </div>
+                </div>
+                <div class="col-lg-10">
+                  <h6>Boosts Creditworthiness: </h6>
+                </div>
+              </div>
+                <p>On-time payments can raise your company's credit score, which can facilitate future loans.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="features-style-box features-style-box-height">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-2">
+                  <div class="icon-style d-flex justify-content-center align-items-center">
+                    <img src="assets/services/msme-loan/icons/business-growth.svg" alt="logo" width="16">
+                  </div>
+                </div>
+                <div class="col-lg-10">
+                  <h6>Business Growth Opportunities: </h6>
+                </div>
+              </div>
+              <p>Capital can be used to take advantage of growth prospects like seasonal inventory accumulation or bulk purchases.   Better financial planning is made possible by predictable interest rates, which are frequently fixed.</p>
+            </div>
+          </div>
+          <div class="col-md-8 col-sm-6" style="height: 225px;margin-top: 0.5%;">
+            <img src="assets/services/msme-loan/working-capital-term-loan/working-capital-term.jpg" style="object-fit: cover; width:100%; height: 100%; border-radius: 4px;">
+          </div>
+        </div>
+    </div>
+    </div>
+  </div>
+</section>
+
+<!-- Eligibility -->
+<section id="eligibility" class="eligibility section">
+  <div class="container">
+  <div class="row">
+    <div class="col-lg-8 col-md-12">
+      <h4 class="h4-size2">Eligibility Criteria</h4>
+      <p>Eligibility for a Working Capital Term Loan can vary between lenders, but common criteria include:
+      </p>
+      <ul>
+          <li>
+            <b>Type of Business:</b> Construction companies, manufacturers, traders, or service providers.
+          </li>
+          <li>
+            <b>Business Vintage:</b> Generally, companies need to have been in operation for a certain amount of time, such as two to three years.   (New businesses also take into account).
+          </li>
+          <li>
+            <b>Annual Turnover:</b> In order to evaluate repayment capacity, lenders frequently demand a minimum annual turnover.
+          </li>
+          <li>
+            <b>Credit Score/CIBIL Score:</b> Approval of a loan depends on a high credit score. 
+          </li>
+          <li>
+            <b>Stability of finances:</b> Proven profitability and sound financial records.
+          </li>
+          <li>Business Type:</b> Some lenders may favor or disqualify particular business kinds.  
+          </li>
+          <li>
+            <b>Industry Type:</b> Certain industries are regarded as more hazardous than others.
+          </li>
+          <li>
+            <b>Debt-to-Equity Ratio:</b> Financial stability is indicated by a strong debt-to-equity ratio. 
+          </li>
+          <li>
+            <b>Collateral (Occasionally):</b> Depending on the loan size and business characteristics, certain lenders may demand collateral.   
+          </li>
+          <li>
+            <b>GST Registration:</b> It is often necessary to provide proof of GST registration.
+          </li>
+      </ul>
+    </div>
+    <div class="col-lg-4 col-md-12 d-sm-none d-md-none d-lg-block mb-4">
+      <img src="assets/services/msme-loan/working-capital-term-loan/eligibility.jpg" alt="" class="w-100">
+    </div>
+  </div>
+</div>
+</section>
+
+<!--  -->
+<!-- document -->
+<section id="document" class="section">
+  <div class="document">
+      <div class="container">
+      <div class="row bg--">
+        <h4 class="h4-size2 mb-3">Documents Required</h4>
+        <div class="col-sm-6">
+          <ul>
+            <li>ID & Address proof</li>
+            <li>PAN Card</li>
+            <li>Business Registration Certificate (GST, Trade License, MSME Registration, etc.)</li>
+            <li>Bank Statements (Last 6–12 Months)</li>
+            <li>Audited Financial Statements (Last 2–3 Years)</li>
+            <li>Income Tax Returns (Last 2–3 Years)</li>
+          </ul>
+        </div>
+        <div class="col-sm-6">
+          <ul>
+          <li>Sales and Purchase Invoices</li>
+          <li>Utility Bill or Rent Agreement (For Address Proof)</li>
+          <li>Passport-size Photograph</li>
+          <li>Property Papers or Asset Ownership Proof</li>
+          <li>Hypothecation Deed (For Secured Loans)</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+   </div>
+</section>
+
+
+<!-- Frequently Asked Questions -->
+<section id="faqs" class="section">
+  <div class="about_loan_area_gradient">
+    <div class="container">
+      <h4 class="h4-size2">Frequently Asked Questions</h4>
+        <div class="accordion mt-5" id="accordionExample">
+          <!-- Accordion Item 1 -->
+          <div class="accordion-item">
+              <h2 class="accordion-header p-1">
+                  <button class="accordion-button collapsed d-flex justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#loanitol_item1">
+                    <img src="assets/home/icons/sm-logo.svg" alt="list-icon" class="me-3">
+                 Can a working capital term loan be used for expansion?
+                      <span class="ms-auto icon">
+                        <i class="fa fa-plus" aria-hidden="true"></i>
+                      </span>
+                  </button>
+              </h2>
+              <div id="loanitol_item1" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    Working capital Term loan  typically used for long-term investments, like purchasing equipment, expanding facilities, or funding business projects. If a business is planning a major expansion or purchasing significant assets, a term loan provides the structured, long-term financing required. 
+                  </div>
+              </div>
+          </div>
+          <!-- Accordion Item 2 -->
+          <div class="accordion-item">
+              <h2 class="accordion-header p-1">
+                  <button class="accordion-button collapsed d-flex justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#loanitol_item2">
+                    <img src="assets/home/icons/sm-logo.svg" alt="list-icon" class="me-3">
+                    What is the maximum tenure for a working capital term loan?
+                      <span class="ms-auto icon">
+                        <i class="fa fa-plus" aria-hidden="true"></i>
+                      </span>
+                  </button>
+              </h2>
+              <div id="loanitol_item2" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    The maximum tenure for a Working Capital Term Loan typically ranges from 5 to 10      years, depending on the lender and loan terms.
+                  </div>
+              </div>
+          </div>
+          <!-- Accordion Item 3 -->
+          <div class="accordion-item">
+            <h2 class="accordion-header p-1">
+                <button class="accordion-button collapsed d-flex justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#loanitol_item3">
+                  <img src="assets/home/icons/sm-logo.svg" alt="list-icon" class="me-3">
+                  What is the cibil score for working capital loan?
+                    <span class="ms-auto icon">
+                      <i class="fa fa-plus" aria-hidden="true"></i>
+                    </span>
+                </button>
+            </h2>
+            <div id="loanitol_item3" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  The minimum cibil required for Working capital erm loan is 700
+                </div>
+            </div>
+          </div>
+          <!-- Accordion Item 4 -->
+          <div class="accordion-item">
+            <h2 class="accordion-header p-1">
+                <button class="accordion-button collapsed d-flex justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#loanitol_item4">
+                  <img src="assets/home/icons/sm-logo.svg" alt="list-icon" class="me-3">
+                  Advantage of working capital Term loan?
+                    <span class="ms-auto icon">
+                      <i class="fa fa-plus" aria-hidden="true"></i>
+                    </span>
+                </button>
+            </h2>
+            <div id="loanitol_item4" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <li>
+                    <b>Lump Sum Funding:</b>
+                    Get a sizable sum of money up front, enabling quick access to finances. 
+                  </li>
+                  <li>
+                    <b>Structured Repayment:</b>
+                    Cash flow management is predictable with fixed monthly or quarterly installments.
+                  </li>
+                  <li>
+                    <b>Adaptable Use:</b>
+                    The money can be applied to a range of working capital requirements, providing operational adaptability.
+                  </li>
+                  <li>
+                    <b>Better Cash Flow:</b>
+                    Take care of immediate cash flow issues and keep business operations running smoothly.
+                  </li>
+                  <li>
+                    <b>Business Growth Opportunities:</b>
+                    Capital can be used to take advantage of growth prospects like seasonal inventory accumulation or bulk purchases.   Better financial planning is made possible by predictable interest rates, which are frequently fixed.
+                  </li>
+                  <li>
+                    <b>Longer Tenure Compared to Overdrafts:</b>
+                    Term loans offer greater time for repayment due to their longer tenures.   
+                  </li>
+                  <li>
+                    <b>Boosts Creditworthiness:</b>
+                    On-time payments can raise your company's credit score, which can facilitate future loans.
+                  </li>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</section>
+
+<!-- free to contact -->
+<section id="contact" class="section">
+  <div class="bg-contact-area">
+   <div class="container">
+    <div class="row d-flex align-items-center">
+        <div class="col-md-9">
+          <h4 class="h4-size">Feel free to get in touch with us</h4>
+          <p>Trusted by many, loved by all – see why people choose Loanitol!</p>
+        </div>
+        <div class="col-md-3">
+          <a href="#" class="btn d-flex align-items-center gap-3">
+            <span>Check Now</span>
+            <div class="circle_white d-flex justify-content-center align-items-center">
+              <img src="assets/home/icons/sm-logo.svg" width="14" alt="">
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+   <?php include("includes/calculation-bottom.php"); ?>
+   <?php include("includes/footer.php"); ?>
+</body>
+</html>
