7a46041aa591999d1a20c28ab7559faa4715eaff
mrudulwazeefa <mrudul@wazeefa.in>
2025-07-13 18:51:35 +0530

    include formatted

--- includes/nav.php
+++ includes/nav.php
@@ -1,96 +1,97 @@
  <nav class="navbar bg-white sticky-top p-3">
-    <div class="container">
-      <a href="index.php">
-        <img src="assets/logo.svg" alt="loanitol-logo" class="logo">
-      </a>
-      <div class="d-flex align-items-center">
-        <span class="navbar-text pe-3">
-          <div class="icon-container">
-            <img src="assets/call.svg" class="icon-h" alt="Call Icon">
-            <img src="assets/whatsapp.svg" class="icon-h" alt="Location Icon">
-            <img src="assets/chat.svg" class="icon-h" alt="Ball Icon">
-          </div>
-          Toll-free Number — <span>
-            <a href="tel:+919090388883"> +91 9090388883 </a></span>
-        </span>
-        <button class="custom-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-          <span class="toggler-icon"></span>
-          <span class="toggler-icon"></span>
-          <span class="toggler-icon"></span>
-        </button>
-      </div>
-      <!-- offcanvas-items -->
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Navigation</h5>
-          <button type="button" class="btn-close custom-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">About Us</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Loan Services
-                <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Advisory Services
-                <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Financial Tools
-                <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Knowledge Base
-                <i class="fa fa-angle-down" aria-hidden="true"></i>
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" role="button">
-                Career
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" role="button">
-                Contact Us
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav>
+     <div class="container">
+         <a href="index.php">
+             <img src="assets/logo.svg" alt="loanitol-logo" class="logo">
+         </a>
+         <div class="d-flex align-items-center">
+             <span class="navbar-text pe-3">
+                 <div class="icon-container">
+                     <img src="assets/call.svg" class="icon-h" alt="Call Icon">
+                     <img src="assets/whatsapp.svg" class="icon-h" alt="Location Icon">
+                     <img src="assets/chat.svg" class="icon-h" alt="Ball Icon">
+                 </div>
+                 Toll-free Number — <span>
+                     <a href="tel:+919090388883"> +91 9090388883 </a></span>
+             </span>
+             <button class="custom-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                 aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                 <span class="toggler-icon"></span>
+                 <span class="toggler-icon"></span>
+                 <span class="toggler-icon"></span>
+             </button>
+         </div>
+         <!-- offcanvas-items -->
+         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+             <div class="offcanvas-header">
+                 <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Navigation</h5>
+                 <button type="button" class="btn-close custom-close" data-bs-dismiss="offcanvas"
+                     aria-label="Close"></button>
+             </div>
+             <div class="offcanvas-body">
+                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-0">
+                     <li class="nav-item">
+                         <a class="nav-link active" aria-current="page" href="#">About Us</a>
+                     </li>
+                     <li class="nav-item dropdown">
+                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                             aria-expanded="false">
+                             Loan Services
+                             <i class="fa fa-angle-down" aria-hidden="true"></i>
+                         </a>
+                         <ul class="dropdown-menu">
+                             <li><a class="dropdown-item" href="#">Action</a></li>
+                             <li><a class="dropdown-item" href="#">Another action</a></li>
+                             <li><a class="dropdown-item" href="#">Something else here</a></li>
+                         </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                             aria-expanded="false">
+                             Advisory Services
+                             <i class="fa fa-angle-down" aria-hidden="true"></i>
+                         </a>
+                         <ul class="dropdown-menu">
+                             <li><a class="dropdown-item" href="#">Action</a></li>
+                             <li><a class="dropdown-item" href="#">Another action</a></li>
+                             <li><a class="dropdown-item" href="#">Something else here</a></li>
+                         </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                             aria-expanded="false">
+                             Financial Tools
+                             <i class="fa fa-angle-down" aria-hidden="true"></i>
+                         </a>
+                         <ul class="dropdown-menu">
+                             <li><a class="dropdown-item" href="#">Action</a></li>
+                             <li><a class="dropdown-item" href="#">Another action</a></li>
+                             <li><a class="dropdown-item" href="#">Something else here</a></li>
+                         </ul>
+                     </li>
+                     <li class="nav-item dropdown">
+                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                             aria-expanded="false">
+                             Knowledge Base
+                             <i class="fa fa-angle-down" aria-hidden="true"></i>
+                         </a>
+                         <ul class="dropdown-menu">
+                             <li><a class="dropdown-item" href="#">Action</a></li>
+                             <li><a class="dropdown-item" href="#">Another action</a></li>
+                             <li><a class="dropdown-item" href="#">Something else here</a></li>
+                         </ul>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" href="#" role="button">
+                             Career
+                         </a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" href="#" role="button">
+                             Contact Us
+                         </a>
+                     </li>
+                 </ul>
+             </div>
+         </div>
+     </div>
+ </nav>
